d6153fdbb0ad75008fee42ca392e32af0110760e
Manas Tamboli <manastamboli2@gamail.com>
2025-08-11 14:49:43 +0530

    first commit

--- lib/database.ts
+++ lib/database.ts
@@ -1,6 +1,7 @@
 import { Pool } from "pg"
-
+console.log("url",process.env.DATABASE_URL)
 const pool = new Pool({
+  
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 })
